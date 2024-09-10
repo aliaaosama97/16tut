bf1f7bca417a90d7b6cc888ad8949993be45a316
Aliaa osama <aliaaosama97@gmail.com>
2024-09-10 12:25:31 +0200

    chapter 16 in progress

---
+++ src/NewPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPost = () => {
+  return (
+    <main>
+      <h1>newPost</h1>
+    </main>
+  )
+}
+
+export default NewPost
---
+++ src/PostPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostPage = () => {
+  return (
+    <main>
+      <h1>postPage</h1>
+    </main>
+  )
+}
+
+export default PostPage
